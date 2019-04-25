59bee5b7d6312e051044bf4d26f074b238d8d12c
Vlad-Andres <bruno993409@gmail.com>
2019-04-25 16:09:27 +0300

    Stable

---
+++ war-app/src/main/java/cf/flymengo/SelfControler.java
@@ -0,0 +1,24 @@
+package cf.flymengo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SelfControler {
+
+	String message = "Welcome to Spring MVC!";
+	
+	@RequestMapping("/testing")
+	public ModelAndView helloWorld(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) 
+	
+	{
+	
+		ModelAndView mv = new ModelAndView("testing");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
